70b5a66b3d2f2000e8665567904542acdbe2e102
Naika JB <jbaptiste.naika@gmail.com>
2024-02-25 16:24:09 -0500

    added missing Console class

---
+++ src/Console.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Console {
+    ArrayList<Flight> flightCatalog;
+    ArrayList<Users> userCatalog;
+    ArrayList<Airport> airportsCatalog;
+
+    public static void main(String[] args) {
+
+    }
+}
